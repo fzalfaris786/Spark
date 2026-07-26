ce5884a5db85dccd588f218906a78f6d4db52544
fzalfaris786 <fzalfaris786@gmail.com>
2026-07-26 20:19:14 +0530

    Delete models/Invite.js

--- models/Invite.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const InviteSchema = new mongoose.Schema({
-    guildId: { type: String, required: true },
-    userId: { type: String, required: true },
-    invites: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('Invite', InviteSchema);
